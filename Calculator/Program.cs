2860c307d91cfa8b23c2ffad788d5beee80b625c
Maria <mariabrock414@gmail.com>
2020-03-03 21:29:18 -0600

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a series of numbers seperated by a comma. Please list them in the order you would like them to be multiplied in.");
+            string userNumbers = Console.ReadLine();
+            string[] splitNumbers = userNumbers.Split(',');
+            //now loop through numbers and multiply together
+            int numSequence = 1;
+            foreach (var number in splitNumbers)
+            {
+                int num = int.Parse(number);
+                numSequence *= num;
+            }
+            Console.WriteLine(numSequence);
+
+            Console.ReadKey();
+        }
+    }
+}
